f500cdfd8b67862e180308e933ded17992c5b41e
shangyindi <2215947984@qq.com>
2020-03-02 17:22:41 +0800

    add fount  commit

---
+++ jcart-administration-fount/viewmodels/product-create.js
@@ -0,0 +1,46 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        productCode: '',
+        productName: '',
+        price: '',
+        discount: '',
+        stockQuantity: '',
+        rewordPoints: '',
+        sortOrder: '',
+        description: '',
+        selectedStatus: 0,
+        statuses: [{
+            value: 0, label: '下架',
+            value: 1, label: '上架',
+            value: 2, label: '待审核',
+        }]
+    },
+    methods: {
+        handleCreateClick() {
+            console.log('create Product')
+            this.createProduct();
+        },
+        createProduct() {
+            axios.post('/product/create', {
+                params: {
+                    productCode: this.productCode,
+                    productName: this.productName,
+                    price: this.price,
+                    discount: this.discount,
+                    stockQuantity: this.stockQuantity,
+                    rewordPoints: this.rewordPoints,
+                    sortOrder: this.sortOrder,
+                    description: this.description,
+                    status: this.status
+                }
+            })
+                .then(function (response) {
+                    console.log('创建成功');
+                })
+                .catch(function (error) {
+                    console.log('创建失败');
+                });
+        }
+    }
+})
--- jcart-administration-fount/viewmodels/product-search.js
+++ jcart-administration-fount/viewmodels/product-search.js
@@ -1,17 +1,30 @@
 var app = new Vue({
     el: '#app',
     data: {
-        pageInfo:''
+        pageInfo:'',
+        pageNum:1
+    },
+    mounted(){
+        console.log('View mounted');
+        //进行赋值
+        this.searchProduct();
     },
     methods:{
+        handlePageChange(val){
+            console.log('page change')
+            this.pageNum = val;
+            this.searchProduct();
+        },
         searchProduct(){
             axios.get('http://localhost:8080/product/search', {
                 params: {
-                   
+                   pageNum:this.pageNum,
+                   pageInfo:this.pageInfo
                 }
             })
                 .then(function (response) {
                     console.log(response);
+                    app.pageInfo = response.data;    
                 })
                 .catch(function (error) {
                     console.log(error);
