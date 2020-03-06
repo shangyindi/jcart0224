5c89919e68abfea6412306a00b1d1583734344e6
shangyindi <2215947984@qq.com>
2020-03-06 20:46:17 +0800

    fix  fount  login customer  commit

--- jcart-store-fount/viewmodels/customer-login.js
+++ jcart-store-fount/viewmodels/customer-login.js
@@ -2,52 +2,35 @@ var app = new Vue({
     el: '#app',
     data: {
         username: '',
-        realName: '',
-        mobile: '',
         password: '',
         respassword: '',
-        email: '',
-        newsSubscribed: false,
-        selectStatus: 0,
-        statuses: [
-            {
-                value: 0,
-                label: '禁用'
-            },
-            {
-                value: 1,
-                label: '启用'
-            },
-            {
-                value: 2,
-                label: '不安全'
-            }
-        ]
     },
     methods: {
-        handleRegisterClick() {
-            console.log('register click')
+        handleLoginClick() {
+            console.log('login click')
             if (this.password != this.respassword) {
                 alert('两次密码不一致')
                 return;
             }
-            this.customerRegister();
+            this.customerLogin();
         },
-        customerRegister() {
-            axios.post('/customer/register', {
-                username: this.username,
-                realName: this.realName,
-                mobile: this.mobile,
-                password: this.password,
-                email: this.email,
-                newsSubscribed: this.newsSubscribed,
-                status:this.selectStatus
+        customerLogin() {
+            axios.get('/customer/login', {
+                params:{
+                    username: this.username,
+                    password: this.password,
+                }
             }).then(function (response) {
-                console.log(response)
-                alert('注册成功');
-            }).catch(function (error) {
-                console.log(error)
+                console.log(response);
+                var dto = response.data;
+                localStorage['jcartToken'] = dto.token;
+                localStorage['expireTimestamp'] = dto.expireTimestamp;
+                console.log(dto);
+                alert('登陆成功');
             })
+                .catch(function (error) {
+                    console.log(error);
+                });
         }
     },
 
