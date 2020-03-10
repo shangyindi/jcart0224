da452e9df594485b7f6736e6f54eec87e0884b50
shangyindi <2215947984@qq.com>
2020-03-10 10:07:15 +0800

    fix  upload  shppingCart commit

--- jcart-store-fount/viewmodels/shopping-cart.js
+++ jcart-store-fount/viewmodels/shopping-cart.js
@@ -11,8 +11,16 @@ var app = new Vue({
     methods: {
         handleDelete(index,row){
             console.log('view deleted click');
+            if(confirm('确认删除？')){
             this.myProducts.splice(index,1);
             localStorage['myShoppingCart'] = JSON.stringify(this.myProducts);
+            this.$message.success('删除购物车成功');
+            }
+        },
+        handleUpdate(index,row){
+            console.log('view upload click')
+            localStorage['myShoppingCart'] = JSON.stringify(this.myProducts);
+            this.$message.success('修改购物车成功');
         }
     },
 
